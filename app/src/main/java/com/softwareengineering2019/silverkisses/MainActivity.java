9a3f7b3aaa8bb21f45886a90fa0025f1f451ba7e
rharris8198 <rh76559p@pace.edu>
2019-10-02 14:15:24 -0400

    Setup basic registration and login system using firebase

--- app/src/main/java/com/softwareengineering2019/silverkisses/MainActivity.java
+++ app/src/main/java/com/softwareengineering2019/silverkisses/MainActivity.java
@@ -1,14 +1,28 @@
 package com.softwareengineering2019.silverkisses;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 }
