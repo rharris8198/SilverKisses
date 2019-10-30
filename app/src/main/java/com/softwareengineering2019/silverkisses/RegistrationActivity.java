f28bd14e50a5ce40c6dcbc2ada6745ac6caf5c36
rharris8198 <rh76559p@pace.edu>
2019-10-30 15:22:38 -0400

    save user firstname, lastname and email to firebase realtime database

--- app/src/main/java/com/softwareengineering2019/silverkisses/RegistrationActivity.java
+++ app/src/main/java/com/softwareengineering2019/silverkisses/RegistrationActivity.java
@@ -16,10 +16,14 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText emailTV, passwordTV;
+    private EditText emailTV, passwordTV, firstNameTV, lastNameTV;
     private Button regBtn;
     private ProgressBar progressBar;
 
@@ -44,9 +48,21 @@ public class RegistrationActivity extends AppCompatActivity {
     private void registerNewUser() {
         progressBar.setVisibility(View.VISIBLE);
 
-        String email, password;
+        final String email, password, firstname, lastname;
         email = emailTV.getText().toString();
         password = passwordTV.getText().toString();
+        firstname = firstNameTV.getText().toString();
+        lastname = lastNameTV.getText().toString();
+
+        if (TextUtils.isEmpty(firstname)) {
+            Toast.makeText(getApplicationContext(), "Please enter first name...", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(lastname)) {
+            Toast.makeText(getApplicationContext(), "Please enter last name...", Toast.LENGTH_LONG).show();
+            return;
+        }
 
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
@@ -67,6 +83,13 @@ public class RegistrationActivity extends AppCompatActivity {
 
                             Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
                             startActivity(intent);
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users");
+                            //build child
+                            ref.child(user.getUid()).setValue(user.getUid());
+                            ref.child(user.getUid()).child("email").setValue(user.getEmail());
+                            ref.child(user.getUid()).child("firstname").setValue(firstname);
+                            ref.child(user.getUid()).child("lastname").setValue(lastname);
                         }
                         else {
                             Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
@@ -79,6 +102,8 @@ public class RegistrationActivity extends AppCompatActivity {
     private void initializeUI() {
         emailTV = findViewById(R.id.email);
         passwordTV = findViewById(R.id.password);
+        firstNameTV = findViewById(R.id.firstName);
+        lastNameTV = findViewById(R.id.lastName);
         regBtn = findViewById(R.id.register);
         progressBar = findViewById(R.id.progressBar);
     }
